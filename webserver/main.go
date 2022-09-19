7156b0e82f15a4ea3f4423950986aa51201c8356
aulianurhady <aulia.nurhady@misteraladin.com>
2022-09-19 17:26:55 +0700

    web server task

---
+++ webserver/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"webserver/service"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	var db []*service.User
+	userSvc := service.NewUserService(db)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/register", userSvc.RegisterHandler)
+	r.HandleFunc("/user", userSvc.GetUserHandler)
+	r.HandleFunc("/user/{id}", userSvc.GetUserHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "localhost:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println("Server running on localhost:8080")
+	_ = srv.ListenAndServe()
+}
