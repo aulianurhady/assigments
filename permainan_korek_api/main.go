b49f138dc1983a51df53937da7779b45cb5d976e
aulianurhady <aulia.nurhady@misteraladin.com>
2022-09-20 23:26:06 +0700

    add permainan correct api

--- permainan_korek_api/main.go
+++ permainan_korek_api/main.go
@@ -6,37 +6,55 @@ import (
 	"time"
 )
 
-func main() {
-	breakpoint := 22
-	rand.Seed(time.Now().UTC().UnixNano())
-	fase := 1
-	for {
-		isEnded := ambilKorek("Player 1", breakpoint)
+type Korek struct {
+	hits       int
+	lastPlayer string
+}
 
-		if isEnded {
-			break
+func main() {
+	lanjut := make(chan *Korek)
+	kalah := make(chan *Korek)
+
+	breakpoint := 10
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for {
+		players := []string{"Player 1", "Player 2", "Player 3", "Player 4"}
+		for _, p := range players {
+			go ambilKorek(p, breakpoint, lanjut, kalah)
 		}
 
-		fase++
+		lanjut <- new(Korek)
+
+		select {
+		case d := <-kalah:
+			fmt.Println(d.lastPlayer, "kalah pada hit ke", d.hits)
+			return
+		}
 	}
 }
 
-func ambilKorek(player string, breakpoint int) (isEnd bool) {
-	time.Sleep(1 * time.Second)
+func ambilKorek(player string, breakpoint int, lanjut, kalah chan *Korek) { //(isEnd bool) {
+
 	min := 1
 	max := 100
+	for {
+		select {
+		case k := <-lanjut:
 
-	nilaiKorek := rand.Intn(max-min+1) + min
+			nilaiKorek := rand.Intn(max-min) + min
+			time.Sleep(500 * time.Millisecond)
 
-	fmt.Printf("%s mendapat nilai %d \n", player, nilaiKorek)
-	result := nilaiKorek % breakpoint
+			k.hits++
+			k.lastPlayer = player
+			fmt.Println("korek ada di", k.lastPlayer, "pada hit ke", k.hits, "dan mempunyai nilai", nilaiKorek)
+			result := nilaiKorek % breakpoint
 
-	if result == 0 {
-		isEnd = true
-		return isEnd
+			if result == 0 {
+				kalah <- k
+				return
+			}
+			lanjut <- k
+		}
 	}
-
-	isEnd = false
-
-	return isEnd
 }
